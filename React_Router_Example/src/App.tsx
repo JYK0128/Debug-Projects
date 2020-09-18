8544e75d7d187851f57bb84b970c9a3dcbcacbc8
Kim Jinyong <wlsyong90@gmail.com>
2020-09-18 14:12:55 +0900

    copy_delete_and_mv
    cant find any solutions

---
+++ React_Router_Example/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navi from "./abc/Navi";
+
+function App() {
+    return (
+        <div className="App">
+            <Navi/>
+        </div>
+    );
+}
+
+export default App;
